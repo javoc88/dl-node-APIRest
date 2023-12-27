84b05138ead16c7d67cd75630f1465f42dab7a51
Javoc <javier.ochoa.77@gmail.com>
2023-12-27 13:43:08 -0300

    Add: se crea consultas.js para conexión con BBDD

---
+++ consultas.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+
+// Configuración de credenciales
+const pool = new Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  allowExitOnIdle: true,
+});
