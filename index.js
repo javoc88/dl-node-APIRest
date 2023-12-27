1a58a5af924a4b49176ce62b3ec35d1d29c56dfa
Javoc <javier.ochoa.77@gmail.com>
2023-12-27 15:36:55 -0300

    Update: se importan funciones desde consultas.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
+const {getJoyas, buildQueryClause} = require("./consultas")
 
 const PORT = process.env.PORT;
 
