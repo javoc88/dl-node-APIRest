0945a1b32fc6d611f89eb25a0e53919082511ea8
Javoc <javier.ochoa.77@gmail.com>
2023-12-27 16:11:51 -0300

    Update: se modifica solicitud GET añadiendo función para HATEOS

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const {getJoyas, buildQueryClauses, prepareHATEOAS} = require("./consultas")
+const { getJoyas, buildQueryClauses, prepareHATEOAS } = require("./consultas");
 
 const PORT = process.env.PORT;
 
 // Solicitud GET
-app.get('/joyas', async (req, res) => {
+app.get("/joyas", async (req, res) => {
   try {
     const queryStrings = req.query;
     const clauses = buildQueryClauses(queryStrings);
     const joyasData = await getJoyas(clauses);
+    const data = prepareHATEOAS(joyasData);
+    res.status(200).send(data);
   } catch (error) {
-    // Manejar errores
     res.status(500).json({ error: error.message });
   }
 });
-
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
