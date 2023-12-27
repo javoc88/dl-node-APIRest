645ac7c43467497bff3a21ba35b22d6279edf106
Javoc <javier.ochoa.77@gmail.com>
2023-12-27 15:33:51 -0300

    Add: se añade función para clausulas

--- consultas.js
+++ consultas.js
@@ -13,6 +13,36 @@ const pool = new Pool({
 
 const getJoyas = async (extras, values) => {
   const consulta = `SELECT * FROM inventario ${extras}`;
-  const { rows } = await pool.query(consulta, values)
-  return rows
+  const { rows } = await pool.query(consulta, values);
+  return rows;
 };
+
+// Cláusulas 
+const buildQueryClause = (queryStrings) => {
+  let clause = "";
+  const { limits, page, order_by } = queryStrings;
+
+  clause = order_by ? `ORDER BY ${order_by.split("_")[0]} ${order_by.split("_")[1]}` : "";
+  clause += limits ? ` LIMIT ${limits}` : "";
+  clause += page && limits ? ` OFFSET ${page * limits - limits}` : "";
+
+  return clause;
+};
+
+
+// const nombreFuncion = (queryStrings) => {
+//   let clausula = "";
+//   const { limits, page, order_by } = queryStrings;
+//   if (order_by) {
+//     const [campo, ordenamiento] = order_by.split("_");
+//     clausula = `ORDER BY ${campo} ${ordenamiento}`;
+//   }
+//   if (limits) {
+//     clausula += " LIMIT " + limits;
+//   }
+//   if (page && limits) {
+//     const offset = page * limits - limits;
+//     clausula += " OFFSET " + offset;
+//   }
+//   return clausula;
+// };
